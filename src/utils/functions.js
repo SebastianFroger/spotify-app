30278a6562558ec248f3d285b8918db017a250fc
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-01-27 20:21:24 +0100

    add auth token retrive and set functions

---
+++ src/utils/functions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// get the access_token, token_type and expires_in, and their values, from url as an object
+export const getUrlParams = (url) => {
+  const token = {};
+
+  url
+    .slice(1) // remove #
+    .split("&")
+    .forEach((item) => {
+      const [title, value] = item.split("=");
+      token[title] = value;
+    });
+
+  return token;
+};
+
+// set token values as default headers in axios calls
+export const setAuthHeader = () => {
+  try {
+    const params = JSON.parse(localStorage.getItem("params"));
+    if (params) {
+      axios.defaults.headers.commons[
+        "Authorization"
+      ] = `Bearer ${params.access}`;
+    }
+  } catch (error) {
+    console.log("Error setting auth token");
+  }
+};
