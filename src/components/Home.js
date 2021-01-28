84037f565d666989412d7cb634b7dc6caec2ea05
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-01-28 20:12:04 +0100

    changes in Home and add store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
+import store from "../store/store";
 //import { Button } from "react-bootstrap";
 import Header from "./Header";
 
-const {
-  REACT_APP_CLIENT_ID,
-  REACT_APP_AUTHORIZE_URL,
-  REACT_APP_REDIRECT_URL,
-} = process.env;
-
-const handleLogin = () => {
-  window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
-};
-
 const Home = (props) => {
+  const {
+    REACT_APP_CLIENT_ID,
+    REACT_APP_AUTHORIZE_URL,
+    REACT_APP_REDIRECT_URL,
+  } = process.env;
+
+  const handleLogin = () => {
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+  };
+
   return (
     <div className="login">
       <Header></Header>
@@ -26,6 +27,4 @@ const Home = (props) => {
   );
 };
 
-// todo: use this when adding store
-// export default connect()(Home);
-export default Home;
+export default connect()(Home);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./styles.css";
 
-// todo: add provider and store (for reducer)
-
-ReactDOM.render(<AppRouter></AppRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  document.getElementById("root")
+);
