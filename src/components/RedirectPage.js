ad58b899f72b2815996ccdd379a766b9c15c48fe
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-01-28 20:40:40 +0100

    get params store params in localstorage route to next page

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,7 +1,24 @@
+import _ from "lodash";
+import { getUrlParams } from "../utils/functions";
 import React from "react";
 
-const RedirectPage = () => {
-  return <div>Redirect Page</div>;
-};
-
-export default RedirectPage;
+export default class RedirectPage extends React.Component {
+  componentDidMount() {
+    const { setExpiryTime, history, location } = this.props;
+    try {
+      if (_.isEmpty(location.hash)) {
+        return history.push("/dashboard");
+      }
+      const access_token = getUrlParams(location.hash);
+      const expiryTime = new Date().getTime() + access_token.expires_in * 1000;
+      localStorage.setItem("params", JSON.stringify(access_token));
+      localStorage.setItem("expiry_time", expiryTime);
+      history.push("/dashboard");
+    } catch (error) {
+      history.push("/");
+    }
+  }
+  render() {
+    return null;
+  }
+}
