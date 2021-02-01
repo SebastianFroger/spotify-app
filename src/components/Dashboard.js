bb5dfdffe121eb7847e659f59db9fe91574c421b
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-02-01 20:54:22 +0100

    Add settings panel and header to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import SearchPanel from "./SearchPanel";
 
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      <Header></Header>
+      <SearchPanel></SearchPanel>
+    </div>
+  );
 };
 
 export default Dashboard;
