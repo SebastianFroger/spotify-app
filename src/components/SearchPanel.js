24d8332d27b63233a4e0f5937947079de1bc1b71
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-02-10 21:06:18 +0100

    move options from searchpanel to listviewOptions

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -8,34 +8,9 @@ export default function SearchPanel() {
     console.log("onDateChange " + date);
   };
 
-  const onAlbumsChange = (checked) => {
-    console.log("onAlbumsChange " + checked);
-  };
-
-  const onSinglesChange = (checked) => {
-    console.log("onSinglesChange " + checked);
-  };
-
-  const onSelectAll = () => {
-    console.log("onSelectAll");
-  };
-
-  const onSelectNone = () => {
-    console.log("onSelectNone");
-  };
-
-  const onSave = () => {
-    console.log("onSave");
-  };
-
   return (
     <div>
-      <Input label="Albums" type="checkbox" onChange={onAlbumsChange}></Input>
-      <Input label="Singles" type="checkbox" onChange={onSinglesChange}></Input>
       <Date label="Since" onChange={onDateChange}></Date>
-      <button onClick={onSelectAll}>Select All</button>
-      <button onClick={onSelectNone}>Select None</button>
-      <button onClick={onSave}>Save</button>
     </div>
   );
 }
