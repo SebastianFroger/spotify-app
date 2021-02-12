2f164c60882130e6d8cb95263ae45292e14dcca0
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-02-12 08:52:42 +0100

    renaming for clarity

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AlbumCard from "./AlbumCard";
 
-export default function ListView() {
+export default function AlbumsList() {
   return (
-    <div className="listview content">
+    <div className="albums-list content">
+      <AlbumCard></AlbumCard>
+      <AlbumCard></AlbumCard>
       <AlbumCard></AlbumCard>
       <AlbumCard></AlbumCard>
       <AlbumCard></AlbumCard>
--- src/components/AlbumsListOptions.js
+++ src/components/AlbumsListOptions.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SearchPanel() {
+export default function AlbumsListOptions() {
   const onAlbumsChange = (checked) => {
     console.log("onAlbumsChange " + checked);
   };
