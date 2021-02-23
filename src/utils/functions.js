834e8f327d2782fd33c292075b1d49ddb1dee425
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-02-23 21:01:37 +0100

    cleanup

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,3 @@
-import axios from "axios";
-
-// get the access_token, token_type and expires_in, and their values, from url as an object
 export const getUrlParams = (url) => {
   const token = {};
 
@@ -13,18 +10,4 @@ export const getUrlParams = (url) => {
     });
 
   return token;
-};
-
-// set token values as default headers in axios calls
-export const setAuthHeader = () => {
-  try {
-    const params = JSON.parse(localStorage.getItem("params"));
-    if (params) {
-      axios.defaults.headers.commons[
-        "Authorization"
-      ] = `Bearer ${params.access_token}`;
-    }
-  } catch (error) {
-    console.log("Error setting auth token");
-  }
 };
