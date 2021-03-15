52734f3b7a12566b6c2b4efeed87a88b3dff379a
sebastian froger <43187719+SebastianFroger@users.noreply.github.com>
2021-03-15 20:29:58 +0100

    albumcard selection highlighting

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as PlayButton } from "../images/play-solid.svg";
 
 export default function AlbumCard(props) {
+  const [selected, setSelected] = useState(false);
+
   const name = props.album.name;
-  const artist = props.album.artistName;
+  const artist = props.album.artists.map((a) => a.name).join();
   const img = props.album.images[1].url;
   const date = props.album.release_date;
 
+  const handleSelect = () => {
+    setSelected((selected) => !selected);
+  };
+
   return (
-    <div className="album-card content">
+    <div
+      className={
+        selected ? "album-card content card-selected" : "album-card content"
+      }
+    >
       <div className="img-container">
-        <img src={img} alt="cover" />
+        <img src={img} alt="cover" onClick={handleSelect} />
       </div>
       <div className="card-bottom">
         <div>
